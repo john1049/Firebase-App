15aee2f45b2a3f77aa03f952fb26e569307f32a5
LAPTOP\Ryan <john1049@algonquinlive.com>
2017-03-16 17:57:20 -0400

    Update source files to most up to date version

--- todo.js
+++ todo.js
@@ -1,43 +1,53 @@
 (function() {
-    var app = angular.module('todoApp', [])
-    app.controller('todoController', function($scope) {
+    .module('todoApp', ['ngAnimate', 'firebase'])
+    .constant('firebaseConfig', {
+      apiKey: "AIzaSyCGz88kNQ2b-7nU8EGaQHtubdnlisG54hI",
+      authDomain: "cloud-based-todo.firebaseapp.com",
+      databaseURL: "https://cloud-based-todo.firebaseio.com",
+      storageBucket: "cloud-based-todo.appspot.com",
+      messagingSenderId: "509749300206"
+    })
+    .run(firebaseConfig => firebase.initializeApp(firebaseConfig))
+    .service('dbRefRoot', DbRefRoot)
+    .service('todos', Todos)
+    .controller('todoController', function($scope) {
         $scope.tasks = (localStorage.getItem('todo') != null) ? JSON.parse(localStorage.getItem('todo')) : [];
         $scope.visible = false;
 
         $scope.update = function() {
             localStorage.setItem('todo', JSON.stringify($scope.tasks));
         }
-				//The unfuck my tasks array
-				//$scope.tasks = [];
-				//$scope.update();
+
+        Array.prototype.remove = function(value) {
+            this.splice(this.indexOf(value), 1);
+            return true;
+        };
 
         $scope.add = function() {
             $scope.todo = ({
                 name: $scope.taskName,
                 date: $scope.dueDate,
-								complete: false
+                complete: false
             })
-						$scope.todo.id = cuid();
-						$scope.tasks.push($scope.todo);
+            $scope.todo.id = cuid();
+            $scope.tasks.push($scope.todo);
             $scope.update();
             $scope.taskName = '';
             $scope.dueDate = '';
         }
 
-        $scope.delete = function() {
-					console.log(this.$index);
-          $scope.tasks.splice(this.$index, 1);
-          $scope.update();
-        }
-
-        $scope.completed = function() {
-						this.task.complete = true;
+        $scope.delete = function(task) {
+            $scope.tasks.remove(task);
             $scope.update();
         }
 
-				$scope.uncomplete = function() {
+        $scope.completed = function() {
+            this.task.complete = true;
+            $scope.update();
+        }
+
+        $scope.uncomplete = function() {
             this.task.complete = false;
-						console.log(this.task.complete);
             $scope.update();
         }
     })
